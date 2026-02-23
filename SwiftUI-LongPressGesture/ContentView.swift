b10ff4189482c94017ec3a5360c93eab6365b28e
Krish Mishra <rm25101980@gmail.com>
2026-02-23 21:18:35 +0530

    Initial Commit

---
+++ SwiftUI-LongPressGesture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-LongPressGesture
+//
+//  Created by Krish Mishra on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-LongPressGesture/SwiftUI_LongPressGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LongPressGestureApp.swift
+//  SwiftUI-LongPressGesture
+//
+//  Created by Krish Mishra on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LongPressGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
